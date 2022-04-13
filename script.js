2bf94bd1d885edf01fa4b69c67ecb72a52926475
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-13 14:05:33 +0200

    v1 is finished, the desing for the mobile verson has yet to be made, but i'll tackle that on another occasion

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-const addButtons = document.querySelector('[data-add]');
+const addButtons = document.querySelectorAll("[data-add]");
+const subButtons = document.querySelectorAll("[data-sub]");
+const teamOneScoreboard = document.querySelector(".team-1");
+const teamTwoScoreboard = document.querySelector(".team-2");
+
+let teamOneScore = 0;
+let teamTwoScore = 0;
 
 addButtons.forEach((addButton) => {
-    addButton.addEventListener("click", () => {
-        const scoringTeam = addButton.dataset.add;
-        console.log(scoringTeam);
-    })
-})
+  addButton.addEventListener("click", () => {
+    const addTeam = addButton.dataset.add;
+    addPoint(addTeam);
+  });
+});
+
+subButtons.forEach((subButton) => {
+  subButton.addEventListener("click", () => {
+    const subTeam = subButton.dataset.sub;
+    subPoint(subTeam);
+  });
+});
+
+function addPoint(team) {
+  if (team == 1) {
+    teamOneScore += 1;
+    teamOneScoreboard.textContent = `${teamOneScore}`;
+  } else if (team == 2) {
+    teamTwoScore += 1;
+    teamTwoScoreboard.textContent = `${teamTwoScore}`;
+  }
+}
+
+function subPoint(team) {
+  if (team == 1) {
+    teamOneScore -= 1;
+    teamOneScoreboard.textContent = `${teamOneScore}`;
+  } else if (team == 2) {
+    teamTwoScore -= 1;
+    teamTwoScoreboard.textContent = `${teamTwoScore}`;
+  }
+}
